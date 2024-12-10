ba55a21d903596f25f2bd761c872e71736e1084c
loki <78568609+faunya@users.noreply.github.com>
2024-12-09 19:38:15 -0500

    fix question routes

--- Kanbas/Questions/routes.js
+++ Kanbas/Questions/routes.js
@@ -3,14 +3,14 @@ import * as questionDao from "./dao.js";
 export default function QuizRoutes(app) {
     app.delete("/api/questions/:questionId", async (req, res) => {
         const { questionId } = req.params;
-        const status = await questionDao.deleteQuiz(questionId);
+        const status = await questionDao.deleteQuestion(questionId);
         res.send(status);
     });
 
     app.put("/api/questions/:questionId", async (req, res) => {
         const { questionId } = req.params;
         const questionUpdates = req.body;
-        const status = await questionDao.updateQuiz(questionId, questionUpdates);
+        const status = await questionDao.updateQuestion(questionId, questionUpdates);
         res.send(status);
     });
 }
